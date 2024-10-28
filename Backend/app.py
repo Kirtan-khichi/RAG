22ba10a5599dc0ea77a8ab6516df7edfb69ee34c
Kirtan-khichi <kirtankk12345@gmail.com>
2024-10-28 17:42:26 +0530

    pandas added to requirnment.txt

--- Backend/app.py
+++ Backend/app.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.config.from_object(Config)
 
-CORS(app, origins=["http://localhost:5173"])
+CORS(app, origins=["http://localhost:5173",  "https://videosearchpadhai.netlify.app"])
 
 db.init_app(app)
 
